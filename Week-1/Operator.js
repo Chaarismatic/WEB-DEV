539ac49a13e3ec985ac5aee3d6f07d7cb0066dc1
Vaibhav Joshi <lynxofficial88@gmail.com>
2026-06-05 17:01:41 +0530

    Operator update

---
+++ Week-1/Operator.js
@@ -0,0 +1,49 @@
+// let a = {
+//   name : "Alexander",
+//   age:36
+// }
+
+// let b = a
+
+// let c = {
+//   name : "Alexander",
+//   age:36
+// }
+
+// console.log(a==b)
+// console.log(a==c)
+// console.log(b==c)
+// console.log(b===c)
+// console.log(b===c)
+// console.log(b===c)
+
+// let a = 10
+// // let b = a
+// let b = 10
+
+// console.log(a==b)
+// console.log(a===b)
+
+// console.log("5"+5)
+
+// console.log(null>=0)
+// console.log(null<=0)
+// console.log(null==0)
+// console.log(null==undefined)
+
+
+// let a = true
+// let b = false
+
+// console.log(a && b)
+
+// let a = 15
+// let b = 14
+
+// let a = 10
+// let b = 15
+let a = 15
+let b = 0
+
+console.log(b&&a)
+console.log(a&&b)
