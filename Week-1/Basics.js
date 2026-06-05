1e591c15e1c9fbb5c17a353f7f28103181d0af96
Vaibhav Joshi <lynxofficial88@gmail.com>
2026-06-05 08:53:15 +0530

    Day-02

---
+++ Week-1/Basics.js
@@ -0,0 +1,72 @@
+// let a = 20
+// let b = 30
+// console.log(a+b)
+
+// let str1 = "Vaibhav"
+// let str2 = 'Joshi'
+// let str3 = `${str1} ${str2}`
+// console.log(str1)
+// console.log(str2)
+// console.log(str3)
+
+// let a
+// console.log(a)
+// console.log(typeof(a))
+
+// let b = null
+// console.log(b)
+// console.log(typeof(b))
+
+// let arr = [1,2,"Vaibhav",45n]
+// console.log(arr)
+// console.log(typeof(arr[3]))
+
+
+// let obj = {
+//   name:"Vaibhav",
+//   age:21,
+//   city:"Haldwani"
+// }
+
+// console.log(obj)
+
+
+// let a = function greet(){
+//   console.log("Hello")
+//   // return 10
+// }
+
+// // let a = greet()
+// console.log(a)
+
+
+// let a = function(){
+//   console.log("Hello")
+// }
+// console.log(a)
+// a()
+
+let a = 10
+let b = 20n
+let c = "vaib"
+let d = Symbol(a)
+let h = Symbol(a)
+let e = true
+let f = null
+let g
+console.log(a) 
+console.log(b) 
+console.log(c) 
+console.log(d) 
+console.log(e) 
+console.log(f) 
+console.log(g) 
+console.log(h) 
+console.log(d==h) // always unique
+console.log(typeof(a)) 
+console.log(typeof(b)) 
+console.log(typeof(c)) 
+console.log(typeof(d)) 
+console.log(typeof(e)) 
+console.log(typeof(f)) 
+console.log(typeof(g)) 
