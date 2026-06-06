2c72adf7e55dc27fcefcf1651e30a92788ff0905
Vaibhav Joshi <lynxofficial88@gmail.com>
2026-06-06 07:55:58 +0530

    day-04

---
+++ Week-1/MathLearn.js
@@ -0,0 +1,38 @@
+// let a = 3
+// let b = 4
+
+// console.log(a&b)
+// console.log(a|b)
+// console.log(a^b)
+// console.log(~a)
+
+
+// let a = 7.4
+// console.log(Math.floor(a))
+// console.log(Math.ceil(a))
+
+// console.log(Math.LN10)
+// console.log(Math.log2(16))
+
+// let a = Math.floor(Math.random()*10000)
+// console.log(a)
+
+// let a = Math.floor(Math.random()*(9999-1000)+1001)
+// console.log(a)
+
+// let str = `hello
+// kal aana`
+// console.log(str)
+
+
+// let str = " abcd  , behind  , dcba    "
+
+// console.log(str.length)
+// console.log(str.includes("c"))
+// console.log(str.indexOf("c"))
+// console.log(str.trim())
+// console.log(str.slice(2,7))
+// console.log(str.substring(2,5))
+// console.log(str.replace('c','d'))
+// console.log(str.replaceAll('c','d'))
+// console.log(str.split(','))
