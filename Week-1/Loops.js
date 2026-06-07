d1078404e5c047eee551105cde2cc16b29065fec
Vaibhav Joshi <lynxofficial88@gmail.com>
2026-06-07 06:44:34 +0530

    LOOP update

---
+++ Week-1/Loops.js
@@ -0,0 +1,24 @@
+// for(let i = 0;i<=5;i++){
+//   console.log(i)
+// }
+
+
+
+// let sum = 0;
+// for(let i = 1;i<=500;i++)
+//   sum+=i;
+// console.log(sum)
+
+
+// for(let i = 65;i<=90;i++){
+//   console.log(String.fromCharCode(i))
+// }
+
+
+for(let i = 0;i<5;i++){
+  let str = ""
+  for(let j = 0;j<=i;j++){
+    str+="*"
+  }
+  console.log(str)
+}
